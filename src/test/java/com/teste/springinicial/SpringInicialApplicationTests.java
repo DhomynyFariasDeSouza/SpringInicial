60b95129ff6d392f96ebd23469ee22029126ea68
Dhomyny Ryan Farias De Souza <dhomyryan@outlook.com>
2024-03-13 09:48:14 -0300

    iniciando o projeto

---
+++ src/test/java/com/teste/springinicial/SpringInicialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.springinicial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInicialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
